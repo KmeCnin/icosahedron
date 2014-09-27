da9ce5d942f2a95da53a6608393dc341e146fb4c
Pierre-Chanel Gauthier <pierrechanel.gauthier@gmail.com>
2014-09-27 02:04:47 +0200

    filtre fonctionnel sur les dons

--- src/Ico/Bundle/RulesBundle/Controller/FeatsController.php
+++ src/Ico/Bundle/RulesBundle/Controller/FeatsController.php
@@ -5,6 +5,7 @@ namespace Ico\Bundle\RulesBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class FeatsController extends Controller
 {
@@ -12,15 +13,8 @@ class FeatsController extends Controller
      * @Route("/rules/feats", name="ico_rules_feats")
      * @Template()
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {	   
-	   $em = $this->get('doctrine.orm.entity_manager');
-	   $dql = "SELECT feat FROM IcoRulesBundle:Feat feat";
-	   $query = $em->createQuery($dql);
-	   $pagination = $this->get('knp_paginator')->paginate(
-		   $query, $this->get('request')->query->get('page', 1) /* page number */, 50 /* limit per page */
-	   );
-	   
 	   $feattypes = $this->getDoctrine()
 		  ->getRepository('IcoRulesBundle:FeatType')
 		  ->findAll();
@@ -28,6 +22,82 @@ class FeatsController extends Controller
 	   $featprerequisites = $this->getDoctrine()
 		  ->getRepository('IcoRulesBundle:FeatPrerequisite')
 		  ->findAll();
+	   
+	   $filter = $this->createFormBuilder()
+			 ->add('name', 'text', array('label' => 'Don', 'required' => false, 'attr' => array('placeholder' => 'Mots-clés')))
+			 ->add('featTypes', 'entity', array(
+				'class' => 'IcoRulesBundle:FeatType',
+				'property' => 'name',
+				'label' => 'Catégories', 
+				'required' => false,
+				'expanded'  => false,
+				'multiple'  => true
+			 ))
+			 ->add('featTypesType', 'choice', array(
+				'choices' => array(
+				    'or' => 'Contien au moins une',
+				    'and' => 'Contien toutes',
+				    'not' => 'Ne contien aucune'
+				),
+				'empty_value' => false,
+				'required' => false
+			 ))
+			 ->add('description', 'text', array('label' => 'Description', 'required' => false, 'attr' => array('placeholder' => 'Mots-clés')))
+			 ->getForm();
+	   $filter->handleRequest($request);
+	   
+	   $queryBuilder = $this->getDoctrine()
+			 ->getRepository('IcoRulesBundle:Feat')
+			 ->createQueryBuilder('feat');
+	   $parameters = array();
+	   
+	   if ($filter->isValid()) {
+		  $data = $filter->getData();
+		  if ($data['featTypesType'] == 'or') {
+			 
+			 $queryBuilder->leftJoin('feat.featTypes', 'type');
+			 foreach ($data['featTypes'] as $key => $type) {
+				$queryBuilder->orWhere('type.id = :type'.$key);
+				$parameters['type'.$key] = $type;
+			 }
+			 
+		  } elseif ($data['featTypesType'] == 'and') {
+			 
+			 $queryBuilder->leftJoin('feat.featTypes', 'type');
+			 foreach ($data['featTypes'] as $key => $type) {
+				$queryBuilder->andWhere('type.id = :type'.$key);
+				$parameters['type'.$key] = $type;
+			 }
+			 
+		  } elseif ($data['featTypesType'] == 'not') {
+			 
+		  }
+		  if (!empty($data['name'])) {
+			 $keywords = explode(' ', $data['name']);
+			 foreach ($keywords as $key => $keyword) {
+				$queryBuilder->orWhere('feat.name LIKE :name'.$key);
+				$parameters['name'.$key] = '%'.$keyword.'%';
+			 }
+		  }
+		  if (!empty($data['description'])) {
+			 $keywords = explode(' ', $data['description']);
+			 foreach ($keywords as $key => $keyword) {
+				$queryBuilder->orWhere('feat.description LIKE :description'.$key);
+				$parameters['description'.$key] = '%'.$keyword.'%';
+			 }
+		  }
+	   }	   
+	   
+	   $queryBuilder->setParameters($parameters);
+//		  var_dump($queryBuilder->getQuery());
+	   
+	   $pagination = $this->get('knp_paginator')->paginate(
+		   $queryBuilder->getQuery(), $this->get('request')->query->get('page', 1) /* page number */, 50 /* limit per page */
+	   );
+	   
+	   if (count($pagination) == 0) {
+		  $this->get('session')->getFlashBag()->add('warning', 'Aucun don ne correspond à vos critères.');
+	   }
 	   
         return $this->render('IcoRulesBundle:Feats:index.html.twig', array(
 		  'breadcrumb' => array(
@@ -39,7 +109,8 @@ class FeatsController extends Controller
 		  'subtitle' => 'Liste',
 		  'feattypes' => $feattypes,
 		  'featprerequisites' => $featprerequisites,
-		  'pagination' => $pagination
+		  'pagination' => $pagination,
+		  'filter' => $filter->createView()
 	   ));
     }
     
